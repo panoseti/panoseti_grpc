3888a5905df28c69e7acd8689c2734854be3bc98
Nicolas Rault-Wang <nraultwang+github@gmail.com>
2026-04-14 23:14:07 -0700

    fix: remove debugging scripts

--- check_pb2.py
+++
@@ -1,7 +0,0 @@
-
-from panoseti_grpc.generated import daq_control_pb2
-print(dir(daq_control_pb2))
-import inspect
-for name, obj in inspect.getmembers(daq_control_pb2):
-    if inspect.isclass(obj):
-        print(f"Class: {name}")
